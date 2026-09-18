ac7a48075b099c25377e468608fedceee75ac963
HuiSik Kim <huisikkim07@gmail.com>
2026-09-18 13:49:19 +0900

    파일제거

--- frontend/src/api/prayer/index.ts
+++
@@ -1,6 +0,0 @@
-// Prayer API 통합 export
-// 기존 import 경로 호환성 유지: import { ... } from '../api/prayer'
-
-export * from './prayerApi'
-export * from './prayerActionApi'
-export * from './replyApi'
--- frontend/src/components/garden/index.ts
+++
@@ -1,5 +0,0 @@
-// 정원 컴포넌트 인덱스
-
-export { LambCharacter } from './LambCharacter'
-export { GrowingTree } from './GrowingTree'
-export { GrowthChart } from './GrowthChart'
